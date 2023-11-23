0c6ad451af3a109269b78e0ece01a5e87ab0f070
Cadam047 <cadam047@fiu.edu>
2023-11-22 20:40:43 -0500

    Done something else 2

--- GUI for TaskOrg/TaskOrgGUI.java
+++ GUI for TaskOrg/TaskOrgGUI.java
@@ -4,26 +4,78 @@ import javax.swing.JFrame;
 import javax.swing.BorderFactory;
 import javax.swing.JPanel;
 import javax.swing.JButton;
+import javax.swing.JLabel;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JTextField;
 
-class TaskOrgGUI {
+
+public class TaskOrgGUI {
+
+    private JLabel label;
+    private JFrame frame;
+    private JPanel panel;
+    private JTextField Text;
+    private JTextField Text2;
+    private JTextField Text3;
 
     public TaskOrgGUI() {
 
-        JFrame frame = new JFrame();
+        frame = new JFrame("Please enter your tasks");
 
-        JButton button = new JButton("Click over here");
+        Text = new JTextField(20);
+        Text2 = new JTextField(20);
+        Text3 = new JTextField(20);
+
+        // Creating Button
+        JButton submitButton = new JButton("Send");
+
+        // Set Action to the button        
+
+        label = new JLabel("");
 
         
-        JPanel panel = new JPanel();
-        panel.setBorder(BorderFactory.createEmptyBorder(30,30,10,30));
-        panel.setLayout(new GridLayout(0, 1));
-        panel.add(button);
+        panel = new JPanel();
 
+        // Creating Border for window
+
+        panel.setBorder(BorderFactory.createEmptyBorder(30,30,10,30));
+
+
+        // Creating the inside of the window
+
+        panel.setLayout(new GridLayout(0, 1));
+        panel.add(Text, BorderLayout.NORTH);
+        panel.add(Text2, BorderLayout.NORTH);
+        panel.add(Text3, BorderLayout.NORTH);
+        panel.add(submitButton, BorderLayout.CENTER);
+        panel.add(label, BorderLayout.SOUTH);
+
+        // Centering the action
         frame.add(panel, BorderLayout.CENTER);
+
+        // Setting the close button 
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        // Setting the title of window 
+
         frame.setTitle("Task Organizer");
         frame.pack();
         frame.setVisible(true);
+
+        submitButton.addActionListener(new ActionListener(){
+
+
+    public void actionPerformed(ActionEvent e) {
+
+        String Input = Text.getText();
+        String Input2 = Text2.getText();
+        String Input3 = Text3.getText();
+        label.setText("User input: " + Input+ ", " + Input2 + ", "  + Input3);
+
+    }
+
+    });
 
     }
     public static void main(String[] args) {
@@ -31,4 +83,5 @@ class TaskOrgGUI {
         new TaskOrgGUI();
 
         }
+
 }
