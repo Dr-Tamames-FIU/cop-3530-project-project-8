ded75d25c80794775a85cc2dbe801edbf2f6b2b9
Cadam047 <cadam047@fiu.edu>
2023-12-03 01:05:45 -0500

    more changes 12/3/2023

--- GUI for TaskOrg/TaskOrgGUI.java
+++ GUI for TaskOrg/TaskOrgGUI.java
@@ -3,7 +3,6 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import java.io.IOException;
-import javafx.application.Application;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -18,6 +17,7 @@ import java.util.Collections;
 import java.util.List;
 
 public class TaskOrgGUI extends Application {
+    
     private final Connection connection = new RotateArray();
 
     @FXML
