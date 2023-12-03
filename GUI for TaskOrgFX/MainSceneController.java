309e41f3747425f62a9bf7118a97f9c0907dd024
Cadam047 <cadam047@fiu.edu>
2023-12-02 19:15:09 -0500

    changes 12/2/2023 2

--- GUI for TaskOrgFX/MainSceneController.java
+++ GUI for TaskOrgFX/MainSceneController.java
@@ -17,20 +17,7 @@ public class MainSceneController {
     private TextField text2;
 
     @FXML
-<<<<<<< HEAD
     private TextField text3; 
-=======
-    private Button organizeButton;
-
-    // angelica RotateArray instance
-    private final Connection connection = new RotateArray();
-
-    private final MostGroup mostGroup = new MostGroup();
-    private final MiddleGroup middleGroup = new MiddleGroup();
-    private final LeastGroup leastGroup = new LeastGroup();
-
-    private Stage stage;
->>>>>>> f0595ae2ac1ea9e404da297224ff052913d575ff
 
     @FXML
     private TextInputControl label;
@@ -42,24 +29,4 @@ public class MainSceneController {
 
     }
 
-<<<<<<< HEAD
-=======
-    public void setStage(Stage stage) {
-        this.stage = stage;
-    }
-
-    private void organizeTasks() {
-        organizeGroup(mostGroup, mostTask1, mostTask2, mostTask3, mostTextArea);
-        organizeGroup(middleGroup, middleTask1, middleTask2, middleTask3, middleTextArea);
-        organizeGroup(leastGroup, leastTask1, leastTask2, leastTask3, leastTextArea);
-    }
-
-    private void organizeGroup(Connection group, TextField task1, TextField task2, TextField task3,
-            TextArea resultArea) {
-        List<String> tasks = Arrays.asList(task1.getText(), task2.getText(), task3.getText());
-        group.sendArrayData(tasks);
-
-        resultArea.setText(group.getArrayData() + ": " + group.getArrayData());
-    }
->>>>>>> f0595ae2ac1ea9e404da297224ff052913d575ff
 }
